3c733724d878d3f5dd1204c27dd97ee6f4ca13cd
eunwk <eunwk@naver.com>
2022-05-11 14:05:39 +0900

    express 샘플 업뎃

--- main.js
+++ main.js
@@ -17,10 +17,11 @@ app.use(morgan('dev')); // log를 찍어줌
 app.use(bodyParser.json()); // 응답받은 json 형태의 파일을 보기 편하게
 app.use('/', express.static('public')); // '/' 경로에 들어왔을때 public 디렉토리의 파일을 쓸수 있도록 처리
 
+// 위의 '/' use 옵션이 있어 아래 명령어는 작동하지 않는다. 같은 경로의 명령어가 있을 경우 위의 명령어가 처리되고, 아래는 무시됨.
 // req 요청, res 응답
-app.get('/', function(req, res) {
-    res.send('Hello World22');
-});
+// app.get('/', function(req, res) {
+//     res.send('Hello World22');
+// });
 
 
 
